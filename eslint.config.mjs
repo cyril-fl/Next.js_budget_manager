f2be0d6e4cf0e9bd6da934f37125f76651416097
cyril-fl <cyril-f@hotmail.com>
2025-03-26 21:14:05 +0100

    feat: init project, set package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,47 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      prettier, // Ajout du plugin Prettier
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "never",
+          distinctGroup: true,
+          warnOnUnassignedImports: false,
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/unified-signatures": "off",
+
+      "no-console": process.env.ENV === "production" ? "warn" : "off",
+      "no-debugger": process.env.ENV === "production" ? "warn" : "off",
+    },
+  },
 ];
 
 export default eslintConfig;
